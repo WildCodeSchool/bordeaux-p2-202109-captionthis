247f7157f02b825371abd1ea2f7a23f1863e25a4
clr-89 <clr.minier@gmail.com>
2021-11-03 22:24:26 +0100

    create profile and item admin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,8 +12,10 @@ class UserController extends AbstractController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $userManager = new UserManager();
             $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $userManager->create($_POST);
-            header('Location: /registration?register=true');
+            $userId = $userManager->create($_POST);
+            $userData = $userManager->selectOneById($userId);
+            $_SESSION['user'] = $userData;
+            header('Location: /profil?id=' . $userId);
         }
         return $this->twig->render('User/register.html.twig', [
             'register_success' => $_GET['register'] ?? null,
@@ -28,8 +30,9 @@ class UserController extends AbstractController
             if (password_verify($_POST['password'], $userData['password'])) {
                 $_SESSION['user'] = $userData;
             } else {
-                var_dump('not ok');
-            }
+                var_dump('échec de la connexion');
+            };
+            header('Location:/');
         }
         return $this->twig->render('User/connect.html.twig', [
         'session' => $_SESSION,
@@ -40,7 +43,7 @@ class UserController extends AbstractController
         session_destroy();
         header('Location: /');
     }
-    public function profil(int $id): string
+    public function profile(int $id): string
     {
         $userManager = new UserManager();
         $userData = $userManager -> selectOneById($id);
