7d64791a49ae5b6d8bb34107b718d502e6a82685
Thomas <thomas.carteron98@gmail.com>
2021-11-04 10:22:02 +0100

    fix bug

--- src/Model/LegendManager.php
+++ src/Model/LegendManager.php
@@ -11,7 +11,7 @@ class LegendManager extends AbstractManager
         $statement = $this->pdo->prepare(" 
        SELECT *, l.id AS legend_id
        FROM legend l
-       JOIN User u 
+       JOIN user u 
        ON l.user_id = u.id
        WHERE picture_id=:id
        ORDER BY ranking DESC");
