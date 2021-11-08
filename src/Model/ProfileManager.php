aa508e89e2c12d2865f8c241281d47a61765aa23
clr-89 <clr.minier@gmail.com>
2021-11-09 00:10:09 +0100

    create front profile.html.twig

--- src/Model/ProfileManager.php
+++ src/Model/ProfileManager.php
@@ -12,6 +12,7 @@ class ProfileManager extends AbstractManager
         $statement = $this->pdo->prepare('
         SELECT * FROM legend
         JOIN user ON legend.user_id = user.id
+        JOIN picture ON legend.picture_id = picture.id
          WHERE user.id =:id
          ORDER BY ranking DESC');
         $statement->bindValue(':id', $id, \PDO::PARAM_STR);
