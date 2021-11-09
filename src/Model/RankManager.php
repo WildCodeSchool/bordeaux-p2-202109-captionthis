7e9b9535903dd95f591c005eddeb0f28bd054d6f
Eleonore <eleonoregouze33@gmail.com>
2021-11-09 16:05:56 +0100

    Create form to post URL pictur in admin page

--- src/Model/RankManager.php
+++ src/Model/RankManager.php
@@ -5,18 +5,23 @@ namespace App\Model;
 class RankManager extends AbstractManager
 {
     public const TABLE = 'legend';
+
     public function selectLegendByRanking(int $id): array
     {
         $statement = $this->pdo->prepare("
         SELECT *, l.id AS legend_id
        FROM legend l
-       JOIN user u 
+       RIGHT JOIN user u 
        ON l.user_id = u.id
        WHERE picture_id=:id
        ORDER BY ranking DESC
        LIMIT 1");
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
-        return $statement->fetch();
+        $result = $statement->fetch();
+        if (!$result) {
+            $result = [];
+        }
+        return $result;
     }
 }
